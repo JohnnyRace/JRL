8148b681abf80616f2162f48a3fafdff6efa4464
John Race <62719622+JohnnyRace@users.noreply.github.com>
2020-04-04 09:31:40 +0300

    Add files via upload

--- firts.sh
+++ firts.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
 
+timedatectl set-ntp true
+loadkeys ru
+ls /sys/firmware/efi/efivars
 pacman-key --init
 pacman-key --populate archlinux
 pacman -Sy
 mkswap /dev/sda2 -L swap
 mkfs.ext4 /dev/sda3 -L root
-mkfs.ext4 /dev/sda4 -L home
 mkfs.fat -F32 /dev/sda1
 mount /dev/sda3 /mnt
-mkdir /mnt/{boot,home}
-mount /dev/sda1 /mnt/boot/
-mkdir /mnt/boot/efi
-mount /dev/sda4 /mnt/home
+mkdir /mnt/boot
+mount /dev/sda1 /mnt/boot
 swapon /dev/sda2
 pacstrap -i /mnt base linux linux-firmware nano base-devel grub networkmanager os-prober dialog wpa_supplicant efibootmgr vim
 genfstab -pU /mnt >> /mnt/etc/fstab
 cp /etc/pacman.d/mirrorlist /mnt/etc/pacman.d/mirrorlist
-arch-chroot /mnt /bin/bash
+arch-chroot /mnt
 umount -R /mnt
 reboot
--- second.sh
+++ second.sh
@@ -1,22 +1,31 @@
 #!/bin/bash
 
+ln -sf /usr/share/zoneinfo/Europe/Minsk /etc/localtime
+hwclock --systohc
 echo "en_US.UTF-8 UTF-8" > /etc/locale.gen
 echo "ru_RU.UTF-8 UTF-8" >> /etc/locale.gen 
 locale-gen
 sleep 1
-ln -sf /usr/share/zoneinfo/Europe/Minsk /etc/localtime
-echo "/dev/sda /    ext4 defaults 0 1" > /etc/fstab
+nano /etc/hostname
+#rocket
+nano /etc/hosts
+#127.0.0.1			localhost
+#::1				localhost
+#127.0.1.1			rocket.localdomain			rocket
+passwd
+useradd -g users -G power.storage.wheel -m rocket
+passwd rocket
+pacman -S efibootmgr
+pacman -S os-prober
+os-prober
 mkinitcpio -P
-grub-install --target=x86_64-efi --efi-directory=/boot/efi --bootloader-id=grub
+grub-install --target=x86_64-efi --efi-directory=/boot/ --bootloader-id=GRUB
 grub-mkconfig -o /boot/grub/grub.cfg
 pacman-key --init
 pacman-key --populate archlinux
-pacman  -Sy  xorg xorg-server  lightdm-deepin-greeter  
+pacman  -Sy  xorg xorg-server 
 pacman  -Sy  plasma
 pacman -Sy plasma-wayland-session.
 pacman -Sy kde-applications 
 sleep 1
-passwd
-useradd -m -g users -s /bin/bash rocket
-passwd rocket
 exit
