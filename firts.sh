2f8282501a3e8f4a24827136fdb75cf9c73cf7dd
John Race <62719622+JohnnyRace@users.noreply.github.com>
2020-04-05 20:11:24 +0300

    Add files via upload

--- firts.sh
+++ firts.sh
@@ -13,7 +13,7 @@ mount /dev/sda3 /mnt
 mkdir /mnt/boot
 mount /dev/sda1 /mnt/boot
 swapon /dev/sda2
-pacstrap -i /mnt base linux linux-firmware nano base-devel grub networkmanager os-prober dialog wpa_supplicant efibootmgr vim
+pacstrap -i /mnt base linux linux-firmware nano base-devel grub networkmanager os-prober dialog wpa_supplicant efibootmgr vim brcmsmac b43-firmware-classic
 genfstab -pU /mnt >> /mnt/etc/fstab
 cp /etc/pacman.d/mirrorlist /mnt/etc/pacman.d/mirrorlist
 arch-chroot /mnt
